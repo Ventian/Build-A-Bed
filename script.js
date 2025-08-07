aec147ba384e72a99beee3d5c9a89b64dc9989fd
Ventian <155928176+Ventian@users.noreply.github.com>
2025-08-06 18:56:28 -0700

    NEw

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 
 const API_URL = "https://api.airtable.com/v0/appDNuN0YvvvBTq0x/Manufacturers";
 const API_KEY =
-  "patQCICTtUh1z62ZT.c9baf2e2a3fc7b3aab90023bc632606c28e86b2bc52297c34bb1566f103f3ca1"; // Do NOT expose in production
-
+  "patQCICTtUh1z62ZT.c9baf2e2a3fc7b3aab90023bc632606c28e86b2bc52297c34bb1566f103f3ca1";
 fetch(API_URL, {
   headers: {
     Authorization: `Bearer ${API_KEY}`,
